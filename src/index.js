e41390aa362fe93af7a7c99438f4af0e6a025446
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-23 11:18:22 +0100

    filter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-var numbers = [3, 56, 2, 48, 5];
-
 //Map -Create a new array by doing something with each item in an array.
 // function Double(x) {
 //   return x * 3;
 // }
+// console.log(numbers.map(Double));
+
 // const newNumbers = numbers.map(function(x){
-//   return x*2;
+//   return x * 2;
 // });
 // console.log(newNumbers);
 
-// 
-function Double(number){
-  return number * 3;
-}
-console.log(numbers.map(Double));
+// var newNumber = [];
+// numbers.forEach(function(x){
+//   newNumber.push(x * 3);
+// });
 
+// console.log(newNumber);
 
-
-
-
-
+var numbers = [3, 56, 2, 48, 5];
 
 //Filter - Create a new array by keeping the items that return true.
+// function New(num){
+//   return num < 10;
+// }
+// const newNumbers = numbers.filter(New);
+// console.log(newNumbers);
+
+// var newNumber = [];
+// numbers.forEach(function (num){
+//   if(num<10){
+//     newNumber.push(num)
+//   }
+// });
+// console.log(newNumber);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
