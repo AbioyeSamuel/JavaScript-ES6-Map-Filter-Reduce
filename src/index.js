3e768c70eb816f7c1cf5aad000a6fae02b67a692
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-23 16:58:02 +0100

    Arrow function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 //Map -Create a new array by doing something with each item in an array.
-// function Double(x) {
-//   return x * 3;
-// }
-// console.log(numbers.map(Double));
+
+var newNumbers1 = numbers.map((x) => x * 1);
+console.log(newNumbers1);
 
 // const newNumbers = numbers.map(function(x){
 //   return x * 2;
@@ -18,11 +17,9 @@
 // console.log(newNumber);
 
 //Filter - Create a new array by keeping the items that return true.
-// function New(num){
-//   return num < 10;
-// }
-// const newNumbers = numbers.filter(New);
-// console.log(newNumbers);
+
+var newNumbers2 = numbers.filter((num) => num < 10);
+console.log(newNumbers2);
 
 // var newNumber = [];
 // numbers.forEach(function (num){
@@ -33,11 +30,11 @@
 // console.log(newNumber);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// function Reduce(accumulator, currentNumber) {
-//   return accumulator + currentNumber;
-// }
-// var newNumber = numbers.reduce(Reduce);
-// console.log(newNumber);
+
+var newNumbers3 = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumbers3);
 
 // var newNumber = 0;
 // numbers.forEach(function (currentNumber) {
@@ -46,24 +43,21 @@
 // console.log(newNumber);
 
 //Find - find the first item that matches from an array.
-// function Find(number){
-//   return number > 10;
-// }
-// var FoundNumbers = numbers.find(Find);
-// console.log(FoundNumbers);
+
+var FoundNumbers = numbers.find((number) => number > 10);
+console.log(FoundNumbers);
 
 //FindIndex - find the index of the first item that matches.
-// function FindIndex(index) {
-//   return index > 10;
-// }
-// var IndexFound = numbers.findIndex(FindIndex);
-// console.log(IndexFound);
 
+var IndexFound = numbers.findIndex((index) => index > 10);
+console.log(IndexFound);
+
+//CHALLENGE
 //An array of meanings and is truncated too 100 characters.
-import emojipedia from "./emojipedia";
-function Emoj(emoji) {
-  return emoji.meaning.substring(0, 100) + "...";
-}
-const emojiMeaning = emojipedia.map(Emoj);
+// import emojipedia from "./emojipedia";
+// function Emoj(emoji) {
+//   return emoji.meaning.substring(0, 100) + "...";
+// }
+// const emojiMeaning = emojipedia.map(Emoj);
 
-console.log(emojiMeaning);
+// console.log(emojiMeaning);
